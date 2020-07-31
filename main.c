8ae790faaf9e2322e1dbf21c34d8c30af37a730c
Guillaume J. Charmes <gcharmes@magicleap.com>
2020-07-30 23:10:56 -0400

    Read loop with prompt and fork/exec of ls

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <wait.h>
 
 /*
@@ -20,25 +21,62 @@ getppid():5    getppid():?
 
  */
 
-int main() {
-  char* buf;
+void  xwrite(int fd, void* buf, int len) {
   int n;
 
+  if ((n = write(fd, buf, len)) < 0) {
+    perror("write:");
+    exit(1);
+  }
+}
+
+void  xputs(void* str) {
+  xwrite(STDOUT_FILENO, str, strlen(str));
+}
+
+int     main() {
+  char* buf;
+  int   n;
+
   // Read from STDIN.
-  if ((buf = malloc(sizeof(char) * 1024)) == NULL) {
+  if ((buf = malloc(sizeof(char) * (1024 + 1))) == NULL) {
     perror("malloc:");
     exit(1);
   }
 
-  while ((n = read(STDIN_FILENO, buf, 1024)) > 0) {
-    if ((write(STDOUT_FILENO, buf, n)) < 0) {
-      perror("write:");
+  while (1) {
+    xputs("(seeshell) ");
+    if (((n = read(STDIN_FILENO, buf, 1024)) < 0)) {
+      perror("read:");
       exit(1);
     }
-  }
-  if (n < 0) {
-    perror("read:");
-    exit(1);
+    if (n == 0) {
+      break;
+    }
+    buf[n] = '\0';
+    /* xputs(buf); */
+
+    /* Exec input. */
+    int pid;
+
+    if ((pid = fork()) == -1) {
+      perror("fork:");
+      exit(1);
+    }
+
+    if (pid == 0) {
+      char* const lsArgs[] = {"/usr/bin/ls", NULL};
+      char* const lsEnv[] = {NULL};
+      if (execve(lsArgs[0], lsArgs, lsEnv) == -1) {
+        perror("execve:");
+        exit(1);
+      }
+    }
+
+    if (wait(&pid) == -1) {
+      perror("wait:");
+      exit(1);
+    }
   }
 }
 
